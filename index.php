9cea321a5957ded6bed0917ebf74a6bfa12b3964
Rayan <rayan@example.com>
2026-09-10 13:48:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="ressources/favicon.ico" />
+    <title>Axe</title>
+    <link href="./styles/index.css" rel="stylesheet" />
+</head>
+<body>
+    <p>Hello world</p>
+</body>
+</html>
