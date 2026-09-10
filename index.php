e6d2c587a694e4186d5289556ea22ac129da0add
Rayan <rayan@example.com>
2026-09-10 14:19:16 +0200

    added login bdd

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
+<?php
+
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=axe', 'root', '');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="ressources/favicon.ico" />
-    <title>Axe</title>
     <link href="./styles/index.css" rel="stylesheet" />
+    <title>Axe</title>
 </head>
-<body>
-    <p>Hello world</p>
+<body oncontextmenu="return false;">
+    <div>
+
+    </div>
 </body>
 </html>
